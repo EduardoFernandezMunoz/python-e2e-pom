fc8acdc5ccf3e18df3f2a7e1a011ad064e0f53ca
Edu <edufermu@gmail.com>
2026-02-21 18:15:03 +0100

    ActionChains added

--- POM_Practice/pageObjects/login.py
+++ POM_Practice/pageObjects/login.py
@@ -1,11 +1,10 @@
 from selenium.webdriver.common.by import By
-from POM_Practice.pageObjects.ecommerce import EcommercePage
-from utils.browserutils import BrowserUtils
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
+from POM_Practice.pageObjects.ecommerce import EcommercePage
+from utils.browserutils import BrowserUtils
 
-# LoginPage handles login screen interactions
 class LoginPage(BrowserUtils):
     def __init__(self, driver):
         super().__init__(driver)
@@ -14,7 +13,7 @@ class LoginPage(BrowserUtils):
         self.show_password_button = (By.XPATH, "//button[@type='button']")
         self.go_to_ecommerce_page = (By.XPATH, "//button[@type='submit']")
 
-    def login(self,username,password):
+    def login(self, username, password):
         # Enter user email
         self.driver.find_element(*self.email_input).send_keys(username)
 
@@ -22,14 +21,24 @@ class LoginPage(BrowserUtils):
         password_field = self.driver.find_element(*self.password_input)
         password_field.send_keys(password)
 
-        # Validate password input is correct
-        password_button_clickable = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.show_password_button))
+        # Click the "show password" button safely
+        # Wait until clickable, scroll into view, then use ActionChains to click
+        password_button_clickable = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.show_password_button)        )
         self.driver.execute_script("arguments[0].scrollIntoView(true);", password_button_clickable)
         ActionChains(self.driver).move_to_element(password_button_clickable).click().perform()
+
+        # Verify the password input is correct
         password_value = password_field.get_attribute("value")
         assert password_value == "Password123", "Password is different from Password123"
 
-        # Submit login and navigate to EcommercePage
-        self.driver.find_element(*self.go_to_ecommerce_page).click()
+        # Click the login/submit button safely
+        # Wait until clickable, scroll into view, then use ActionChains to click
+        submit_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.go_to_ecommerce_page)
+        )
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", submit_button)
+        ActionChains(self.driver).move_to_element(submit_button).click().perform()
+
+        # Return the EcommercePage object
         ecommerce_page = EcommercePage(self.driver)
         return ecommerce_page
