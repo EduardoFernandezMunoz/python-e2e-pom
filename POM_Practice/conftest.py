8c8127a902b09929b7b2dd9d5a54a0ddc92380b6
Edu <edufermu@gmail.com>
2026-02-21 17:53:29 +0100

    Headless mode

--- POM_Practice/conftest.py
+++ POM_Practice/conftest.py
@@ -12,9 +12,19 @@ def browser_instance(request):
     browser_name = request.config.getoption("browser_name")
 
     if browser_name == "chrome":
-        driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Ejecuta Chrome sin UI
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
+        driver = webdriver.Chrome(options=chrome_options)
+
     elif browser_name == "firefox":
-        driver = webdriver.Firefox()
+        from selenium.webdriver.firefox.options import Options as FirefoxOptions
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument("--headless")
+        driver = webdriver.Firefox(options=firefox_options)
 
     driver.maximize_window()
     driver.implicitly_wait(5)
