3215f39b2560c937608576eedb8b55b10931be1e
Edu <edufermu@gmail.com>
2026-02-21 20:20:59 +0100

    Absolute path

--- POM_Practice/conftest.py
+++ POM_Practice/conftest.py
@@ -7,27 +7,28 @@ def pytest_addoption(parser):
         "--browser_name", action="store", default="chrome", help="browser selection"
     )
 
-
 @pytest.fixture
 def browser_instance(request):
     browser_name = request.config.getoption("browser_name")
 
-    if browser_name == "chrome":
+    if browser_name.lower() == "chrome":
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Ejecuta Chrome sin UI
+        chrome_options.add_argument("--headless=new")  # Headless moderno, más estable
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--window-size=1920,1080")  # Ventana grande para que todo sea clickeable
         driver = webdriver.Chrome(options=chrome_options)
 
-    elif browser_name == "firefox":
+    elif browser_name.lower() == "firefox":
         from selenium.webdriver.firefox.options import Options as FirefoxOptions
         firefox_options = FirefoxOptions()
         firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--width=1920")
+        firefox_options.add_argument("--height=1080")
         driver = webdriver.Firefox(options=firefox_options)
 
-    driver.maximize_window()
+    # Tiempo de espera implícito
     driver.implicitly_wait(5)
 
     yield driver
