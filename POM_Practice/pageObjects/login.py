f986cdad9b08ac35d20606b7d0bfb3582d622ac9
Edu <edufermu@gmail.com>
2026-02-21 18:20:05 +0100

    Safe_click added

--- POM_Practice/pageObjects/login.py
+++ POM_Practice/pageObjects/login.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from POM_Practice.pageObjects.ecommerce import EcommercePage
@@ -13,6 +12,12 @@ class LoginPage(BrowserUtils):
         self.show_password_button = (By.XPATH, "//button[@type='button']")
         self.go_to_ecommerce_page = (By.XPATH, "//button[@type='submit']")
 
+    def safe_click(self, locator):
+        """Wait for element to be clickable, scroll to center, and click via JavaScript"""
+        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator))
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+        self.driver.execute_script("arguments[0].click();", element)
+
     def login(self, username, password):
         # Enter user email
         self.driver.find_element(*self.email_input).send_keys(username)
@@ -21,25 +26,15 @@ class LoginPage(BrowserUtils):
         password_field = self.driver.find_element(*self.password_input)
         password_field.send_keys(password)
 
-        # --- Click the "show password" button safely ---
-        password_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.show_password_button)
-        )
-        # Scroll to the element and click using ActionChains
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", password_button)
-        ActionChains(self.driver).move_to_element(password_button).click().perform()
+        # Click the "show password" button safely
+        self.safe_click(self.show_password_button)
 
         # Verify password field
         password_value = password_field.get_attribute("value")
         assert password_value == "Password123", "Password is different from Password123"
 
-        # --- Click the login/submit button safely ---
-        submit_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.go_to_ecommerce_page)
-        )
-        # Scroll to the center of the button to avoid sticky headers
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", submit_button)
-        ActionChains(self.driver).move_to_element(submit_button).click().perform()
+        # Click the login/submit button safely
+        self.safe_click(self.go_to_ecommerce_page)
 
         # Return the EcommercePage object
         return EcommercePage(self.driver)
